e8279d9771cda09fa560422f166517c689905e07
Protyay_Ray <115409416+Protyay140@users.noreply.github.com>
2023-06-12 01:02:35 +0530

    Update Institute.java

--- assignment 2/Institute.java
+++ assignment 2/Institute.java
@@ -1,3 +1,9 @@
+// For every person in an institute details like name, address (consists of premises number, street, city,
+// pin and state), phone number, e-mail id are maintained. A person is either a student or a faculty. For
+// student roll number and course of study are also be maintained. For faculty employee id, department
+// and specialisation are to be stored. One should be able to view the object details and set the attributes.
+// For address, one may change it partially depending on the choice. Design and implement the classes.
+
 import java.util.Scanner;
 
 class Person{
